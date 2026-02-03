585e4194334e86f80de57476181cfacc97e0bc61
André Lynum <andre.lynum@spk.no>
2026-02-03 18:59:39 +0100

    Test flow with message

--- test_flow.py
+++ test_flow.py
@@ -1,9 +1,11 @@
 from prefect import flow
+from prefect.logging import get_run_logger
 
 
 @flow
-def test_workflow() -> str:
-    print("Runnning test")
+def test_workflow(message: str = "what!") -> str:
+    l = get_run_logger()
+    l.info(f"Runnning test with {message}")
     return "Work flow test"
 
 
